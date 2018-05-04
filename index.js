dc8682994d210c020a9c97d259084e069c7fe734
Aris Husanu <abane94@gmail.com>
2018-05-03 19:10:23 -0500

    updates

--- index.js
+++ index.js
@@ -1,20 +1,75 @@
-function post(a, b, c) {
-    var text = $('#edit').val();
-    var html_string = render(text);
-    var post = createEl(html_string);
-    $('#cardList').append(post);
+var userName;
+
+$( document ).ready(function() {
+    userName = localStorage.getItem('name');
+    // alert(name);
+    $('#welcomeBar').text += ' ' + userName;
+    if (!userName || userName === "null") {
+        $('#newModal').modal();
+        userName = prompt("Please enter you name: ");
+        localStorage.setItem('name', userName);
+    }
+
+    var ls = [
+        {
+            name: 'John Smith',
+            text: 'Working on that 395 project',
+        },
+        {
+            name: 'Jane Doe',
+            text: 'If i pretend hard enough classes will be over right?'
+        },
+        {
+            name: 'Obi Wan',
+            text: 'Hello There'
+        }
+    ];
+
+    ls.forEach(post => {
+        addPost(post.name, post.text);
+    });
+
+    $('#edit').keypress(function(e) {
+        if(e.which == 13) {
+            $('postBtn').click();
+        }
+    });
+});
+
+
+function post() {
+    var text = $('#edit').val().trim();
+    if (!text) { return; }
+    var post = addPost(userName, text, true);
+
+    setTimeout(function() {
+        post.parentNode.removeChild(post);
+        $('#postModal').modal();
+    }, 1700);
 }
 
-function render(text) {
+function addPost(name, text, prepend) {
+    var html_string = render(name, text);
+    var post = createEl(html_string);
+    if (prepend) {
+        $('#cardList').prepend(post);
+    } else {
+        $('#cardList').append(post);
+    }
+    post.classList+= ' small';
+    return post;
+}
+
+function render(name, text) {
     return`
-        <div class="card">
+        <div class="card post">
             <div class="card-body">
-                <h4 class="card-title">John Doe</h4>
-                <p class="card-text">
+                <h4 class="card-title">${name}</h4>
+                <p class="card-text post-text">
                     ${text}
                 </p>
-                <a href="#!" class="card-link">Like</a>
-                <a href="#!" class="card-link">Share</a>
+                <button type="button" class="btn btn-primary">Like</button>
+                <button type="button" class="btn btn-dark">Share</button>
             </div>
         </div>
         `
